fe25cd85ccd03d87c85f9a0a33ddfe5fa1a23bd5
TIJK <htming1993@gmail.com>
2016-07-21 18:56:18 +0800

    finish models.py

--- www/models.py
+++ www/models.py
@@ -1,15 +1,18 @@
-import time, uuid
+import time,uuid
+from www.orm import Model, StringField, BooleanField, FloatField, TextField
 
-from orm  import Model, StringField, BooleanField, FloatField, TextField
 
+# 自增函数
 def next_id():
+    print('time.time()----',time.time())
+    print('uuid.uuid4()----', uuid.uuid4())
     return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
+
 class User(Model):
-    """docstring for User"""
     __table__ = 'users'
 
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    id = StringField(primary_key=True,default=next_id,ddl='varchar(50)')
     email = StringField(ddl='varchar(50)')
     passwd = StringField(ddl='varchar(50)')
     admin = BooleanField()
@@ -17,11 +20,11 @@ class User(Model):
     image = StringField(ddl='varchar(500)')
     created_at = FloatField(default=time.time)
 
+
 class Blog(Model):
-    """docstring for Blog"""
     __table__ = 'blogs'
 
-    id =StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    id = StringField(primary_key=True,default=next_id,ddl='varchar(50)')
     user_id = StringField(ddl='varchar(50)')
     user_name = StringField(ddl='varchar(50)')
     user_image = StringField(ddl='varchar(500)')
@@ -30,20 +33,14 @@ class Blog(Model):
     content = TextField()
     created_at = FloatField(default=time.time)
 
+
 class Comment(Model):
-    """docstring for Comment"""
     __table__ = 'comments'
 
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    id = StringField(primary_key=True,default=next_id,ddl='varchar(50)')
     blog_id = StringField(ddl='varchar(50)')
     user_id = StringField(ddl='varchar(50)')
     user_name = StringField(ddl='varchar(50)')
     user_image = StringField(ddl='varchar(500)')
-    content = TextField()
+    comment = TextField()
     created_at = FloatField(default=time.time)
-    #test
-        
-
-        
-
-        
